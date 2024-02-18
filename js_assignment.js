b897e4931ff93b78cf86b3ebf8b93967f0876af7
sonali-salve <sonalisalve10012002@gmail.com>
2024-02-18 16:34:36 +0530

    add task 7 files

---
+++ js_assignment.js
@@ -0,0 +1,184 @@
+// Assignment 1:
+// Write a JavaScript function that takes a number as a parameter and prints whether it's positive, negative, or zero. 
+
+function checkNum(num){
+    if(num>0){
+        console.log(`Number ${num} is positive`);
+    }
+    else if(num<0){
+        console.log(`Number ${num} is negative`);
+    }
+    else{
+        console.log(`Number ${num} is Zero`);
+    }
+}
+checkNum(0); //function calling
+
+
+// Assignment 2:
+// Write a JavaScript function that takes a positive integer as a parameter and calculates its factorial using a for loop. 
+// The factorial of a number N is the product of all positive integers less than or equal to N. 
+function factorial(num){
+    let result=1;
+    for(let i=1;i<=num;i++){
+        result=result*i;
+    }
+    console.log(`factorial of giiven number is ${result}`);
+}
+let n=prompt("Enter a number : ");
+factorial(n); //function calling
+
+
+// Assignment 3:
+// Write a JavaScript function that takes two numbers as parameters and returns the larger one. 
+function largerNum(num1,num2){
+    return num1 > num2 ? num1 : num2 ; //tenory operator
+}
+let result=largerNum(44,11);  //function calling and final result store in variable
+console.log(`largest number is ${result}`);
+
+
+//  Assignment 4:
+//  Write a JavaScript function that takes a string as a parameter and determines whether it's a palindrome or not. 
+// A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward 
+// (ignoring spaces, punctuation, and capitalization). 
+function palindromeStr(str){
+    let reverse='';
+
+    for(let i=(str.length)-1;i>=0;i--){
+        reverse+=str[i];
+    }
+    if(reverse==str){
+        console.log(`String ${str} is palindrome`);
+    }
+    else {
+        console.log(`String ${str} is not palindrome`);
+    }
+}
+let str=prompt("Enter a string : ");
+palindromeStr(str);
+
+
+// Assignment 5:
+// Write a JavaScript function that takes a positive integer as a parameter and prints all the prime numbers less than or equal to that integer. 
+// A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers. 
+function primeNum(num){
+    let count;
+    for(let i=2;i<=num;i++){
+        count=0;
+        for(j=1;j<=num;j++){
+            if(i%j==0){
+                count++;
+            }
+        }
+        if(count==2){
+            console.log(`Number ${i} is a prime number`);
+        }
+    }
+}
+primeNum(5); //function calling
+
+// Assignment 6:
+// Write a JavaScript function that simulates a simple calculator. 
+// The function should take two numbers and an operator (+, -, *, or /) as parameters and perform the corresponding operation. 
+function calculator(num1,num2,operator){
+    let result;
+    switch(operator){
+        case '+':
+            result=num1+num2;
+            console.log(`sum of number is ${result}`);
+            break;
+        case '-':
+            result=num1-num2;
+            console.log(`subtraction of number is ${result}`);
+            break;
+        case '*':
+            result=num1*num2;
+            console.log(`multiplication of number is ${result}`);
+            break;
+        case '/':
+            result=num1/num2;
+            console.log(`division of number is ${result}`);
+            break;
+        default :
+            console.log("invailid input");
+            break;
+    }
+}
+calculator(85,45,'+');  //function calling
+
+// Assignment 7:
+// Write a JavaScript function that takes a string as a parameter and counts the number of vowels (a, e, i, o, u) in the string. 
+function checkVowels(str){
+    let count=0;
+    for(let i=0;i<str.length;i++){
+    switch(str[i]){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            count++;
+            break;
+        }
+    }
+    return count;
+
+}
+console.log(`Number of vowels is ${checkVowels("javascript tutorial")}`);
+
+
+// Assignment 8:
+// Write a JavaScript function that takes a positive integer as a parameter and checks if it's a perfect number.
+//  A perfect number is a positive integer that is equal to the sum of its proper divisors, excluding itself. 
+
+function perfectNum(num){
+    let sum=0;
+    for(let i=1;i<num;i++){
+        if(num%i==0){
+            sum+=i;
+        }
+    }
+    if(sum==num){
+        console.log(`Number ${num} is perfect number`);
+    }
+    else {
+        console.log(`Number ${num} is not perfect number`);
+    }
+}
+perfectNum(7); //function calling
+
+
+// Assignment 9:
+// Write a JavaScript function that takes a number as a parameter and prints the Fibonacci series up to that number. 
+// The Fibonacci series is a sequence of numbers in which each number is the sum of the two preceding ones. 
+function fibonacciSer(value){
+    let a,b;
+    a=0;
+    b=1;
+
+    for(let i=1;i<=value;i++){
+        console.log(a);
+        let temp = a+b;
+        a=b;
+        b=temp;
+    }
+}
+fibonacciSer(10);  //function caliing
+
+
+
+// Assignment 10:
+// Write a JavaScript function that takes a positive integer as a parameter and prints its multiplication table up to 10. 
+function table(num){
+    for(let i=1;i<=10;i++){
+       console.log(num+"*"+i+"="+num*i);
+    }
+}
+table(4); //function calling
+
+
+
+
+
+
